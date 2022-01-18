d6ae38890981bdd2ff489f1040ca8f9a042f6745
Bao Phung <b.giaphung@gmail.com>
2022-01-18 10:54:03 -0600

    attempting to connect to api

--- bank-app/src/api/agent.ts
+++ bank-app/src/api/agent.ts
@@ -1,4 +1,15 @@
+import axios from 'axios';
 import React from 'react';
+import { store } from '../stores/store';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+axios.interceptors.request.use((config) => {
+    const token = store.commonStore.token;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+})
+
 
 
 const agent = {
---
+++ bank-app/src/stores/commonStore.ts
@@ -0,0 +1,26 @@
+import { makeObservable, reaction } from "mobx";
+import React from "react";
+
+export default class CommonStore {
+    token: string | null = null;
+
+    constructor() {
+        makeObservable(this);
+
+        reaction(
+            () => this.token,
+            (token) => {
+                if (token) {
+                    window.localStorage.setItem('jwt', token);
+                } else {
+                    window.localStorage.removeItem('jwt');
+                }
+            }
+        );
+    }
+
+    setToken = (token: string | null) => {
+        if (token) window.localStorage.setItem('jwt', token);
+        this.token = token;
+    }
+}
